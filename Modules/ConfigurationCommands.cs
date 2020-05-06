1eb7b1081f043e22dc7c2b26698232827a3a663b
Ethan O'Donnell <ethan@ejpo.io>
2020-05-06 14:48:27 +0100

    Workaround to reduce omnisharp warnings

--- Modules/ConfigurationCommands.cs
+++ Modules/ConfigurationCommands.cs
@@ -10,22 +10,26 @@ namespace SaucyBot.Modules
         [Command("configReportChannel")]
         async Task SetReportChannelAsync()
         {
+            await Console.Out.WriteLineAsync("No Implementation for this yet");
             throw new NotImplementedException();
         }
 
         [Command("configTicketingChannel")]
         async Task SetTicketingChannelAsync()
         {
+            await Console.Out.WriteLineAsync("No Implementation for this yet");
             throw new NotImplementedException();
         }
         [Command("configAddRole")]
         async Task AddAdministaraiveRole()
         {
+            await Console.Out.WriteLineAsync("No Implementation for this yet");
             throw new NotImplementedException();
         }
         [Command("configRemoveRole")]
         async Task RemoveAdministrativeRole()
         {
+            await Console.Out.WriteLineAsync("No Implementation for this yet");
             throw new NotImplementedException();
         }
     }
