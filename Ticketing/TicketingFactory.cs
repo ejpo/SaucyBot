98ff214560fcfb0afb3652a1f6cb1820aa192310
Ethan O'Donnell <ethan@ejpo.io>
2020-05-06 13:37:18 +0100

    Created a factory to handle async construction for ticket objects

---
+++ Ticketing/TicketingFactory.cs
@@ -0,0 +1,20 @@
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace SaucyBot.Ticketing
+{
+   public class TicketingFactory
+   {
+       public TicketingFactory()
+       {
+           //Constructor shit goes here
+       }
+
+       public async Task<Responder> CreateResponderAsync(SocketUser identity, SocketGuild guild)
+       {
+           Responder newResponder = new Responder(identity, guild);
+           await newResponder.Initialization;
+           return newResponder;
+       }
+   }
+}
