795b99c479a001b56216c7c38e05311077b9da1e
Ethan O'Donnell <ethan@ejpo.io>
2020-05-07 12:24:50 +0100

    Fleshed out the saucy bot team class

--- Ticketing/Team.cs
+++ Ticketing/Team.cs
@@ -1,13 +1,70 @@
-using Discord.WebSocket;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
+using System;
+using SaucyBot.Interfaces;
 namespace SaucyBot.Ticketing
 {
-    class Team
+    public class Team : IAsyncIntialization
     {
-        private int identifier;
-        private string name;
-        private SocketGuild guild;
-        private List<Responder> _responders;
+        private ulong _teamID;
+        private String _teamName;
+        private ulong _guildID;
+        private Dictionary<ulong,Responder> _teamResponders;
+
+        public Team(string name, ulong guildID)
+        {
+            Initialization = CreateAsync(name, guildID);
+        }
+
+        private Task<bool> CreateAsync(string name, ulong guildID)
+        {
+            _teamName = name;
+            _guildID = guildID;
+            _teamID = DeriveTeamID();
+            _teamResponders = new Dictionary<ulong, Responder>();
+
+            return Task.FromResult<bool>(true);
+        }
+
+        private ulong DeriveTeamID()
+        {
+            ulong uid = 57;
+            uid = uid * ulong.Parse(_teamName.ToCharArray()) / 12;
+            uid = uid * _guildID / 12;
+
+            return uid;
+        }
+
+        public Task<bool> AddResponder(Responder responder)
+        {
+            try
+            {
+                if( !(_teamResponders.ContainsKey(responder.SaucyID)) ){
+                    _teamResponders.Add(responder.SaucyID, responder);
+                }
+                return Task.FromResult<bool>(true);
+            }
+            catch (System.Exception)
+            {
+                return Task.FromResult<bool>(false);
+            }
+        }
+
+        public Task<bool> RemoveResponder(Responder responder)
+        {
+            try
+            {
+                if(_teamResponders.ContainsKey(responder.SaucyID)){
+                    _teamResponders.Remove(responder.SaucyID);
+                }
+                return Task.FromResult<bool>(true);
+            }
+            catch (System.Exception)
+            {
+                return Task.FromResult<bool>(false);
+            }
+        }
+
+        public Task<bool> Initialization { get; private set; }
     }
 }
