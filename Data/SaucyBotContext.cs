e36c6c0084af6e0f33557d287d6cfe5bfd88d2e7
Ethan O'Donnell <ethan@ejpo.io>
2021-02-03 19:24:42 +0000

    Created EF Core Database Context

---
+++ Data/SaucyBotContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace SaucyBot.Data
+{
+    class SaucyBotContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options) { 
+            => options.UseSqlite("Data Source=SaucyBot.db");
+        }
+    }
+}
